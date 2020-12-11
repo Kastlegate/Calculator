0736030cec245a4dd92d49753c5f9e03c411b1ef
Kasey <Kastlegate@gmail.com>
2020-12-11 15:24:06 -0500

    Finished a good portion of the calculator's layout

---
+++ calculator.js
@@ -0,0 +1,24 @@
+function add(number1, number2)
+{
+    return number1 + number2;
+}
+
+function subtract(number1, number2)
+{
+    return number1 - number2;
+}
+
+function multiply(number1, number2)
+{
+    return number1 * number2;
+}
+
+function divide(number1, number2)
+{
+    return number1 / number2;
+}
+
+console.log(add(30,2))
+console.log(subtract(1000,2))
+console.log(multiply(123456789,2))
+console.log(divide(30,2))
